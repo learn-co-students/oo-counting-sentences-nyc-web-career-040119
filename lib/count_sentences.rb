9eaeaec5cb15f56a27d9e8e7e58f8405356c2ac5
Jacob Lam <jlam@binghamton.edu>
2019-04-03 16:51:06 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.split("").last == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.split("").last == "?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.split("").last == "!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    self.split.count do |char|
+      #binding.pry
+      char.split("").last == "?" || char.split("").last == "." || char.split("").last == "!"
+    end
 
   end
+
 end
